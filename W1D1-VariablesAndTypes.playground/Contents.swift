65c8e27bd2aca751b760d16878eac4c8d46883af
Nathan Hsu <nathan.hsu@icloud.com>
2017-02-20 20:36:23 -0800

    Challenge Done

--- W1D1-VariablesAndTypes.playground/Contents.swift
+++ W1D1-VariablesAndTypes.playground/Contents.swift
@@ -26,14 +26,14 @@ var count = 0
 
 count = 10
 
-//: *Note:* To create a new variable we write `var` before the name, when we change the data inside an existing variable we do not.
+//: *Note:* To create a new variable we write `var` before the name, when we change the data inside an existing variable we do not write 'var'.
 
 
 //: ### Challenge 1
 //:
 //: Now it's your turn. Change `greeting` to be a different string by using the assignment operator.
 
-
+greeting = "Suh, Dude!"
 
 //: *Note:* unlike math, where `x = 10` and `10 = x` are interchangeable, in programming the variable goes on the left of the `=` symbol, and the new value goes on the right.
 
@@ -42,8 +42,12 @@ count = 10
 //:
 //: We want to add 1 to whatever the value of `count` is now. We could manually look at the value in `count`, see that it is 10 and put 11 into `count` using the assignment operator. This works, but we can do it in a better way. We can set count to be whatever count is, plus 1. See if you can figure out the syntax for this.
 
+count = count + 1
 
 //: *Note:* If you're ever unsure of the contents of a variable in a playground, write the variable on a line by itself and you will see its value printed to the right of it. Or you can use the `print` function like so: `print(variableName)`.
+
+count
+print(count)
 
 
 //: ## Types
@@ -71,10 +75,12 @@ count
 var tipAmount = 6.01
 // tipAmount's type is:
 
+// Answer: Double
 
 var isOpen = true
 // isOpen's type is:
 
+// Answer: Bool
 
 //: ## Type Inference
 //:
@@ -88,6 +94,7 @@ var height: Double = 10
 //: Since all our variables have types, even if we haven't specifically set them, we can't store any value in them... only values of the right type.
 //: Test this out. Try setting the value of `count` to be `height`:
 
+// count = height
 
 
 //: It didn't work, right? They are both numbers, but `count` is an Int, and `height` is a `Double`. There are ways to convert the `Double` 10 into the `Int` 10, but we won't go into that just now.
@@ -99,10 +106,23 @@ var height: Double = 10
 //:
 //: Write out the name of a variable, e.g. `greeting`, and then a period. This will show the autocomplete menu. From this list you can pick `uppercased` and `lowercased` to change the case of the characters in our greeting.
 //:
+
+greeting.uppercased()
+greeting = greeting.uppercased()
+print(greeting)
+
+
+
+
+
 //: Print out the uppercase and lowercase versions of the string below:
 
 var schoolName = "Lightouse Labs"
+schoolName.uppercased()
+schoolName.lowercased()
 
+print(schoolName.lowercased())
+print(schoolName.uppercased())
 
 
 //: THE END
